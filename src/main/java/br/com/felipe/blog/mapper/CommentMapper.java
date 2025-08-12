daf6ca7b130437ddfb2d5294a5abf450ee176034
André Lucas <andre.lucas.mendes01@aluno.ifce.edu.br>
2025-08-12 15:23:11 -0300

    refact: Update Comments DTOs to better fit expected values with postId

--- src/main/java/br/com/felipe/blog/mapper/CommentMapper.java
+++ src/main/java/br/com/felipe/blog/mapper/CommentMapper.java
@@ -1,6 +1,7 @@
 package br.com.felipe.blog.mapper;
 
 import br.com.felipe.blog.dto.CommentRequestDTO;
+import br.com.felipe.blog.dto.CommentResponseDTO;
 import br.com.felipe.blog.entity.Comment;
 import br.com.felipe.blog.entity.Post;
 import org.springframework.stereotype.Component;
@@ -8,16 +9,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class CommentMapper {
 
-    public CommentRequestDTO toCommentRequestDTO(Comment comment){
-        return new CommentRequestDTO(
-                comment.getContent()
-        );
+    public CommentResponseDTO toResponseDTO(Comment comment) {
+        return new CommentResponseDTO(
+                comment.getId(),
+                comment.getContent(),
+                comment.getPost().getId(),
+                comment.getCreatedAt(),
+                comment.getUpdatedAt());
     }
 
-    public Comment toEntity(CommentRequestDTO commentRequestDTO, Post post){
+    public Comment toEntity(CommentRequestDTO commentRequestDTO, Post post) {
         return new Comment(
                 commentRequestDTO.getContent(),
-                post
-        );
+                post);
     }
 }
